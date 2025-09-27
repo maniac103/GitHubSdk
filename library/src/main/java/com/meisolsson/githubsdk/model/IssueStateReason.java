126ae0119a6aa2f828a5d9fca062e609fd784238
Fs00 <francescosaltori@gmail.com>
2025-09-27 17:55:59 +0200

    Add "duplicate" issue state reason

--- library/src/main/java/com/meisolsson/githubsdk/model/IssueStateReason.java
+++ library/src/main/java/com/meisolsson/githubsdk/model/IssueStateReason.java
@@ -5,5 +5,6 @@ import com.squareup.moshi.Json;
 public enum IssueStateReason {
     @Json(name = "reopened") Reopened,
     @Json(name = "completed") Completed,
-    @Json(name = "not_planned") NotPlanned
+    @Json(name = "not_planned") NotPlanned,
+    @Json(name = "duplicate") Duplicate
 }
